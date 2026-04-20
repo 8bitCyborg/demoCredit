14451cff7fcbd17bb1f0017824e4a158b3ac73f2
Shalom Junior <shalomjunior007@gmail.com>
2026-04-20 10:55:33 +0100

    .

--- src/services/loans.service.ts
+++ src/services/loans.service.ts
@@ -19,7 +19,7 @@ export class LoanService {
   async analyzeLoanApplication(loanAmount: string, installments: number, file: any) {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
     const model = genAI.getGenerativeModel({
-      model: "gemini-3-pro-preview", //"gemini-2.5-flash",
+      model: "gemini-2.5-flash",
       generationConfig: { responseMimeType: "application/json" },
     });
 
